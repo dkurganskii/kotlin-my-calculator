dbf71dafd5f2c930ca57d58931956f198696612f
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2022-12-19 07:01:55 -0600

    finalize the app

--- app/src/main/java/com/dk/mycalculator/MainActivity.kt
+++ app/src/main/java/com/dk/mycalculator/MainActivity.kt
@@ -38,9 +38,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onOperator(view: View){
+    fun onOperator(view: View) {
         tvInput?.text?.let {
-            if(lastNumeric && !isOperatorAdded(it.toString())){
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
                 tvInput?.append((view as Button).text)
                 lastNumeric = false
                 lastDot = false
@@ -48,31 +48,65 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onEqual(view: View){
-        if(lastNumeric){
+    fun onEqual(view: View) {
+        if (lastNumeric) {
             var tvValue = tvInput?.text.toString()
             var prefix = ""
             try {
-                if(tvValue.startsWith("-")){
+                if (tvValue.startsWith("-")) {
                     prefix = "-"
                     tvValue = tvValue.substring(1)
                 }
-                if(tvValue.contains("-")){
+                if (tvValue.contains("-")) {
                     val splitValue = tvValue.split("-")
                     var one = splitValue[0]
                     var two = splitValue[1]
 
-                    if(prefix.isNotEmpty()){
+                    if (prefix.isNotEmpty()) {
                         one = prefix + one
                     }
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
 
-                    tvInput?.text = (one.toDouble() - two.toDouble()).toString()
+                } else if (tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                } else if (tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                } else if (tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
                 }
 
-            }catch (e : ArithmeticException){
+            } catch (e: ArithmeticException) {
                 e.printStackTrace()
             }
         }
+    }
+
+    private fun removeZeroAfterDot(result: String): String {
+        var value = result
+        if(result.contains(".0"))
+            value = result.substring(0, result.length -2)
+        return value
     }
 
     private fun isOperatorAdded(value: String): Boolean {
